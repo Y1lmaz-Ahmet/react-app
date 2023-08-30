61270ae17308884fe0983a6bcb23adb582a7bc0a
y1lmaz-Ahmet <ay0201998@gmail.com>
2023-08-30 15:10:47 +0200

    fetching data with hooks from github api

--- src/App.js
+++ src/App.js
@@ -1,31 +1,16 @@
 import "./App.css";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
-  const [title, setTitle] = useState("");
-  const [color, setColor] = useState("#000000");
-  const submitForm = (e) => {
-    e.preventDefault();
-    alert(`title:${title} color:${color}`);
-    setTitle("");
-    setColor("#000000");
-  };
-  return (
-    <form onSubmit={submitForm}>
-      <input
-        type='text'
-        placeholder='color title...'
-        value={title}
-        onChange={(event) => setTitle(event.target.value)}
-      />
-      <input
-        type='color'
-        value={color}
-        onChange={(event) => setColor(event.target.value)}
-      />
-      <button>add</button>
-    </form>
-  );
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch(`https://api.github.com/users/y1lmaz-ahmet`)
+      .then((response) => response.json())
+      .then(setData);
+  }, []);
+  if (data) return <pre>{JSON.stringify(data, null, 2)}</pre>;
+
+  return <h1>DATA:</h1>;
 }
 
 export default App;
